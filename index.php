c93a780114e88dcd1a7210aa12f87ac8ccc08ecc
jesmesjugger <martinjj80@gmail.com>
2020-04-06 13:33:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,25 +20,24 @@ if ($text == "") {
 } else if ($text == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
-    $response = "END Your phone number is "";
+    $response = "END Your phone number is ".$phoneNumber;
 
 } else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
-//$accountNumber  = "ACC1001";
+    $accountNumber  = "ACC1001";
 
     // This is a terminal request. Note how we start the response with END
-    $response = "END Your account number is "";
+    $response = "END Your account number is ".$accountNumber;
 
 } else if ( $text == "1*2" ) {
     // This is a second level response where the user selected 1 in the first instance
-//     $balance  = "KES 10,000";
+    $balance  = "KES 10,000";
 
     // This is a terminal request. Note how we start the response with END
-    $response = "END Your balance is "";
+    $response = "END Your balance is ".$balance;
 }
 
 // Echo the response back to the API
 header('Content-type: text/plain');
-echo $responses
+echo $response;
 ?>
-
