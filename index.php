ef2a260e63ac465590c62cd758ace772aa5ba826
jesmesjugger <martinjj80@gmail.com>
2020-04-08 19:27:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ else if ( $text == "3*2" ) {
     // This is a terminal request. Note how we start the response with END
     $response = "END What is Covid-19".$covidInfo;
 }
-elseif ($ussd_string_exploded[0]) {
+else ($ussd_string_exploded[0]) {
    return [0]:
 }
 
