ef15eb6fc16515b256986be97b8b13b14369110d
jesmesjugger <martinjj80@gmail.com>
2020-04-08 11:07:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -80,7 +80,8 @@ else if ( $text == "3*2" ) {
     // This is a terminal request. Note how we start the response with END
     $response = "END What is Covid-19".$covidInfo;
 }
-else {$response  = "CON Welcome to OldMutual Kindly Select one Option \n";
+else if( $text == "0" ) {
+    $response  = "CON Welcome to OldMutual Kindly Select one Option \n";
     $response .= "1. My Account \n";
     $response .= "2. My phone number \n";
     $response .= "3. Get Covid-19 Updates";
