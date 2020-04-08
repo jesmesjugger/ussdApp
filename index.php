5749df7b378dba8a4a8b54bd65ed221c77dd66eb
jesmesjugger <martinjj80@gmail.com>
2020-04-08 11:09:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -66,6 +66,12 @@ else if ( $text == "1*3*2" ) {
     // This is a terminal request. Note how we start the response with END
     $response = "END Your Account type will be ".$account1." Please visit any Nearest Faulu Bank ";
 }
+else if( $text == "3*0" ) {
+    $response  = "CON Welcome to OldMutual Kindly Select one Option \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number \n";
+    $response .= "3. Get Covid-19 Updates";
+     }
 else if ( $text == "3*1" ) {
     // This is a second level response where the user selected 1 in the first instance
     $covidNews  = "Kenya records 14 more Covid-19 cases, 3 other recoveries";
