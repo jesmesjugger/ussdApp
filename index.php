3568662569dc211763626aaee6e099e7caa2267c
jesmesjugger <martinjj80@gmail.com>
2020-04-10 12:22:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,22 +7,15 @@ $text        = $_POST["text"];
 $ussd_string_exploded = explode("*", $text);
         // Get ussd menu level number from the gateway
         $level = count($ussd_string_exploded);
-        $exploded_2 = explode("0*",$text);
-if( count($exploded_2) == 0 ) {
-$refreshed_text = $text;
-}
-else {
-$refreshed_text = $exploded_2[count($exploded_2)-1];
-}
 
-if ($refreshed_text == "" ) {
+if ($text == "" ) {
     // This is the first request. Note how we start the response with CON
     $response  = "CON Welcome to OldMutual Kindly Select one Option \n";
     $response .= "1. My Account \n";
     $response .= "2. My phone number \n";
     $response .= "3. Get Covid-19 Updates";
 
-} else if ($refreshed_text == "1") {
+} else if ($text == "1") {
     // Business logic for first level response
     $response = "CON Choose account information you want to view \n";
     $response .= "1. Account number \n";
@@ -30,37 +23,33 @@ if ($refreshed_text == "" ) {
     $response .= "3. Open New Account \n";
     $response .= "0. HOME";
 
-
-    
-   
-
-}else if ($refreshed_text == "3") {
+}else if ($text == "3") {
         // Business logic for first level response
         $response = "CON Choose your prefered Covid-19  \n";
         $response .= "1. Get  Current News On Covid-19 \n";
         $response .= "2. Get information on Safety ways from Covid-19 \n";
 }
-else if($refreshed_text == "1*1") { 
+else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
     $accountNumber  = "ACC1001";
 
     // This is a terminal request. Note how we start the response with END
     $response = "END Your account number is ".$accountNumber;
 
-} else if ( $refreshed_text == "1*2" ) {
+} else if ( $text == "1*2" ) {
     // This is a second level response where the user selected 1 in the first instance
     $balance  = "KES 10,000";
 
     // This is a terminal request. Note how we start the response with END
     $response = "END Your balance is ".$balance;
-}else if ($refreshed_text == "1*3") {
+}else if ($text == "1*3") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
     $response = "CON Choose type of account register \n";
     $response .= "1. Mobile Banking \n";
     $response .= "2. Physical Bank";
 }
-elseif ($refreshed_text == "1*3*1") {
+elseif ($text == "1*3*1") {
     // when use response with option django
     $response = "CON Please enter your first name";
 }
@@ -82,43 +71,35 @@ elseif ($ussd_string_exploded[0] == 1 && $ussd_string_exploded[1] == 3 && $level
             $response = "END Your data has been captured successfully! Thank you for opening bank account with Faulu Bank";
         }
 
-else if ( $refreshed_text == "1*3*2" ) {
+else if ( $text == "1*3*2" ) {
     // This is a second level response where the user selected 1 in the first instance
     $account1  = "Physical Bank";
 
     // This is a terminal request. Note how we start the response with END
     $response = "END Your Account type will be ".$account1." Please visit any Nearest Faulu Bank ";
 }
-else if ($refreshed_text == "2") {
+else if ($text == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
     $response = "END Your phone number is ".$phoneNumber;
 }
-else if ( $refreshed_text == "3*1" ) {
+else if ( $text == "3*1" ) {
     // This is a second level response where the user selected 1 in the first instance
     $covidNews  = "Kenya records 14 more Covid-19 cases, 3 other recoveries";
 
     // This is a terminal request. Note how we start the response with END
     $response = "END Latest News ".$covidNews;
 }
-else if ( $refreshed_text == "3*2" ) {
+else if ( $text == "3*2" ) {
     // This is a second level response where the user selected 1 in the first instance
     $covidInfo = "is an infectious disease caused by a new virus.The disease causes respiratory illness (like the flu) with symptoms such as a cough, fever, and in more severe cases, difficulty breathing";
 
     // This is a terminal request. Note how we start the response with END
     $response = "END What is Covid-19".$covidInfo;
-}    if ($refreshed_text!=="1" && $refreshed_text!=="2"){
-    $response  = "CON Welcome to OldMutual Kindly Select one Option \n";
-    $response .= "1. My Account \n";
-    $response .= "2. My phone number \n";
-    $response .= "3. Get Covid-19 Updates";
-   // reset($text);
+}    if ($text=="1*0"){
+        $response="END  ...";
+        reset($level);
 }
-
-
-
-    
-
 // Echo the response back to the API
 header('Content-type: text/plain');
 echo $response;
